a15dc2d2046854b3e593fc3ce7c62d1793a7324f
Tiago Victor de Oliveira <tiagovictordeoliveira@gmail.com>
2023-08-20 22:48:09 -0300

    Adicionar arquivos de projeto.

---
+++ FanoutConsumer/Program.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+IConnection connection;
+IModel channel;
+
+ConnectionFactory connectionFactory = new ConnectionFactory();
+connectionFactory.HostName = "localhost";
+connectionFactory.VirtualHost = "/";
+connectionFactory.Port = 5672;
+connectionFactory.UserName = "guest";
+connectionFactory.Password = "guest";
+
+connection = connectionFactory.CreateConnection();
+channel = connection.CreateModel();
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (sender, args) =>
+{
+    string message = Encoding.UTF8.GetString(args.Body.Span);
+    Console.WriteLine("Message: " + message);
+    
+    
+    channel.BasicAck(args.DeliveryTag, false); // Reconhece e não faz o requeu
+
+    // channel.BasicNack(args.DeliveryTag, false, true); Não reconhece mas faz o requeue
+    channel.BasicNack(args.DeliveryTag, false, false); // Não reconhece e não faz o requeu
+};
+
+var consumerTag = channel.BasicConsume("my.queue1", false, consumer);
+
+Console.WriteLine("Waiting for messages. Press any key to exite");
+Console.ReadKey();
---
+++ FanoutPublisher/Program.cs
@@ -0,0 +1,69 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+IConnection connection;
+IModel channel;
+
+ConnectionFactory connectionFactory = new ConnectionFactory();
+connectionFactory.HostName = "localhost";
+connectionFactory.VirtualHost = "/";
+connectionFactory.Port = 5672;
+connectionFactory.UserName = "guest";
+connectionFactory.Password = "guest";
+
+connection = connectionFactory.CreateConnection();
+
+channel = connection.CreateModel();
+
+channel.ExchangeDeclare(
+    "ex.fanout",
+    "fanout",
+    true,
+    false,
+    null);
+
+channel.QueueDeclare(
+    "my.queue1",
+    true,
+    false,
+    false,
+    null);
+
+channel.QueueDeclare(
+    "my.queue2",
+    true,
+    false,
+    false,
+    null);
+
+channel.QueueBind(
+    "my.queue1",
+    "ex.fanout",
+    "");
+
+channel.QueueBind(
+    "my.queue2",
+    "ex.fanout",
+    "");
+
+channel.BasicPublish(
+    "ex.fanout",
+    "",
+    null,
+    Encoding.UTF8.GetBytes("Ola mundo"));
+
+channel.BasicPublish(
+    "ex.fanout",
+    "",
+    null,
+    Encoding.UTF8.GetBytes("Ola mundo2"));
+
+Console.WriteLine("Press a key to exit");
+Console.ReadKey();
+
+channel.QueueDelete("my.queue1");
+channel.QueueDelete("my.queue2");
+channel.ExchangeDelete("ex.fanout");
+
+channel.Close();
+connection.Close();
