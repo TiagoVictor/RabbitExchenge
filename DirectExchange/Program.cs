b677331ebc5c4b89b13e8aa205e586ebe91fc203
Tiago Victor de Oliveira <tiagovictordeoliveira@gmail.com>
2023-08-21 01:24:07 -0300

    Criando Direct exchange

---
+++ DirectExchange/Program.cs
@@ -0,0 +1,66 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+IConnection connection;
+IModel channel;
+
+ConnectionFactory factory = new ConnectionFactory();
+
+factory.HostName = "localhost";
+factory.VirtualHost = "/";
+factory.Port = 5672;
+factory.UserName = "guest";
+factory.Password = "guest";
+
+connection = factory.CreateConnection();
+channel = connection.CreateModel();
+
+channel.ExchangeDeclare(
+    "ex.direct",
+    "direct",
+    true,
+    false,
+    null);
+
+channel.QueueDeclare(
+    "my.infos",
+    true,
+    false,
+    false,
+    null);
+
+channel.QueueDeclare(
+    "my.errors",
+    true,
+    false,
+    false,
+    null);
+
+channel.QueueDeclare(
+    "my.warnings",
+    true,
+    false,
+    false,
+    null);
+
+channel.QueueBind("my.infos", "ex.direct", "info");
+channel.QueueBind("my.warnings", "ex.direct", "warning");
+channel.QueueBind("my.errors", "ex.direct", "error");
+
+channel.BasicPublish(
+    "ex.direct",
+    "info",
+    null,
+    Encoding.UTF8.GetBytes("My info"));
+
+channel.BasicPublish(
+    "ex.direct",
+    "error",
+    null,
+    Encoding.UTF8.GetBytes("My error"));
+
+channel.BasicPublish(
+    "ex.direct",
+    "warning",
+    null,
+    Encoding.UTF8.GetBytes("My warning"));
